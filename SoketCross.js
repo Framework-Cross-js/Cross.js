1bf84e3916e1a48042d82850e42d6ff98d7d8542
E5war5IT <91418370+E5war5IT@users.noreply.github.com>
2021-12-12 20:11:20 +0300

    Add files via upload

---
+++ SoketCross.js
@@ -0,0 +1,11 @@
+import { Electron } from './Cross';
+import { Electro } from './Cross';
+
+
+console.log(tabularData)
+
+Electron.message('container');
+Electro.message('container');
+
+Electron.launch();
+Electron.config(); 
